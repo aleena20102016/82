882337f052b2c09f0c458d19cf36ce8b3b3f3498
aleena20102016 <88888878+aleena20102016@users.noreply.github.com>
2022-11-05 16:42:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("My first console project");
+
+            var first_name = "Ranbir";
+            var last_name = "Kapoor";
+
+            var full_name = first_name.concat(last_name);
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+
+            var full_name = first_name.concat(last_name);
+            console.log(full_name);
+
+            var add = a+b;
+            console.log(add);
+
+            var sub = a-b;
+            console.log(sub);
+
+            var multi = a*b;
+            console.log(multi);
+
+            var divide = a/b;
+            console.log(divide);
